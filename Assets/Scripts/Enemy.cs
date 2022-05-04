18fe8f5f64c8146181d27125ec4804cbddc3b54f
Pierre <pierreb.montagne@gmail.com>
2022-05-04 01:03:52 -0400

    Enemy Follow

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,20 +6,30 @@ public class Enemy : MonoBehaviour
 {
     public AudioSource source;
     public AudioClip clip;
+    public Transform target;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        source.PlayOneShot(clip);
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (collision.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(clip);
+            sprite.enabled = false;
+            Destroy(gameObject, 1.2f);
+        }
+        
     }
 }
