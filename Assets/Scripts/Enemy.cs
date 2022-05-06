3004d5b3e496750f82d9b11968c79dce62cf83be
dgb1110110 <104702290+dgb1110110@users.noreply.github.com>
2022-05-05 23:00:53 -0400

    Fixed initial speed of enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0;
+        speed = 1;
     }
 
     // Update is called once per frame
