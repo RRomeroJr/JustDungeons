addbe9da7761e00527b28d1f7a1a46d1bf4931c2
dgb1110110 <117.rromero@gmail.com>
2022-05-15 16:26:37 -0400

    Richie Smooth camera movement. Reverted MAX_JUMP_SPEED

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset = new Vector3(0.0f, 2.75f, -10.0f);
+
+    void Start(){
+        //offset = new Vector3(target.transform.position.x, target.transform.position.y + 2.75f, -10f);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+
+        transform.position = smoothedPos;
+    }
+}
