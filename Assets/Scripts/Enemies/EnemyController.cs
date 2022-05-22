9291d6cf3ef6fda8d44e117002ba57c8808da84f
Pierre <pierreb.montagne@gmail.com>
2022-05-22 17:27:46 -0400

    Enemy controller created

---
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    public EnemySO enemySO;
+
+    [Header("Debug Settings")]
+    public float currentHP;
+
+    [Header("Optional Components")]
+    [SerializeField] internal EnemyMovementScript movementScript;
+    [SerializeField] internal EnemyWeaponScript weaponScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
