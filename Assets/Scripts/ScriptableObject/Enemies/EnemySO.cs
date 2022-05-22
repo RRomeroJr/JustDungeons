4a1b786cb376522b89883ab35ec168025fcda882
Pierre <pierreb.montagne@gmail.com>
2022-05-22 16:22:27 -0400

    Skeleton SO

---
+++ Assets/Scripts/ScriptableObject/Enemies/EnemySO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemySO : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+
+    public FloatReference maxHP;
+    public FloatReference moveSpeed;
+}
