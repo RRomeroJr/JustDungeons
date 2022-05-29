fcb67775c66af3d61618a75acc3eb3570a028512
dgb1110110 <117.rromero@gmail.com>
2022-05-29 16:28:03 -0400

    RR: tickRate works but buggy. Cleaned up+Comments

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -27,67 +27,140 @@ public class Actor : MonoBehaviour
     }
 
     void handleSpellEffects(){
+
         if(activeSpellEffects.Count > 0){
 
             for(int i = 0; i < activeSpellEffects.Count; i++){
 
                 switch(activeSpellEffects[i].getEffectType()){
                     case 0: // damage
-                        if(activeSpellEffects[i].getDuration() <= 0.0f){
-                            Debug.Log("Damaging " + actorName + ": " + activeSpellEffects[i].getPower().ToString());
-                            health -= (int)activeSpellEffects[i].getPower();
-                            activeSpellEffects[i].elaspedTime = activeSpellEffects[i].getDuration();// kinda unnecesary but for saftey to make sure it gets deleted
-                        }else{
-                            //Debug.Log("Applying DoT to " + actorName + ": " + activeSpellEffects[i].getPower().ToString());
-                            health -= (int)((Time.deltaTime / activeSpellEffects[i].getDuration()) * activeSpellEffects[i].getPower());
-                            activeSpellEffects[i].elaspedTime += Time.deltaTime;
-                            if(activeSpellEffects[i].elaspedTime % activeSpellEffects[i].getTickRate() == 0.0f){
-                                //Debug.Log("Ticking Dmg " + actorName + ": " + activeSpellEffects[i].getPower().ToString());
-                                health -= (int)((activeSpellEffects[i].getTickRate()/ activeSpellEffects[i].getDuration()) * activeSpellEffects[i].getPower());
-                                activeSpellEffects[i].elaspedTime += Time.fixedDeltaTime;
-                            }
-                        }
+                        handleDamage(activeSpellEffects[i]);
+
                         break;
 
                     case 1: // heal
-                        if(activeSpellEffects[i].getDuration() <= 0.0f){
-                            Debug.Log("Damaging " + actorName + ": " + activeSpellEffects[i].getPower().ToString());
-                            health += (int)activeSpellEffects[i].getPower();
-                            activeSpellEffects[i].elaspedTime = activeSpellEffects[i].getDuration(); // kinda unnecesary but for saftey to make sure it gets deleted
-                        }else{
-                            health += (int)((Time.deltaTime/ activeSpellEffects[i].getDuration()) * activeSpellEffects[i].getPower());
-                            activeSpellEffects[i].elaspedTime += Time.deltaTime;
-                        }
+                        handleHeal(activeSpellEffects[i]);
                         break;
-                    case 2: // something else
+                    case 2: // DoT
+                        if(activeSpellEffects[i].lastTick >= activeSpellEffects[i].getTickRate()){
+                            // if rdy to tick
+
+                            handleDoT(activeSpellEffects[i]);
+                            Debug.Log("Actor: ticking" + activeSpellEffects[i].getEffectName() + " on " + actorName);
+                        }
+                        //Iterate duration
+                        activeSpellEffects[i].duration -= Time.deltaTime;
+                        //Iterate lastTick
+                        activeSpellEffects[i].lastTick += Time.deltaTime;
+                        break;
+                    case 3: // HoT
+                        if(activeSpellEffects[i].lastTick >= activeSpellEffects[i].getTickRate()){
+                            // if rdy to tick
+
+                            handleHoT(activeSpellEffects[i]);
+                            Debug.Log("Actor: ticking" + activeSpellEffects[i].getEffectName() + " on " + actorName);
+                        }
+                        //Iterate duration
+                        activeSpellEffects[i].duration -= Time.deltaTime;
+                        //Iterate lastTick
+                        activeSpellEffects[i].lastTick += Time.deltaTime;
+                        break;
                     default:
                         Debug.Log("Unknown spell type on " + actorName + "! Don't know what to do! Trying to remove..");
-                        activeSpellEffects[i].elaspedTime = activeSpellEffects[i].getDuration();
+                        activeSpellEffects[i].duration = 0.0f;
                         break;
                 
                 }
-            checkSEToRemoveAtPos(activeSpellEffects[i], i);
+            checkASEToRemoveAtPos(activeSpellEffects[i], i);
             }
         //Debug.Log(actorName + " cleared all spell effects!");
         }
     }
-    void takeDamage(int amount){
+    void damageValue(int amount){
+        // Right now this only damages health, but, maybe in the future,
+        // This could take an extra param to indicate a different value to "damage"
+        // For ex. a spell that reduces maxHealth or destroys mana
+
         health -= amount;
-    }
-    float RoundToNearestHalf(float value)
-    {
-        return MathF.Round(value * 2) / 2;
+
     }
 
-    void checkSEToRemoveAtPos(ActiveSpellEffect inASE, int listPos){
-        if(inASE.elaspedTime >= inASE.getDuration()){
+    void restoreValue(int amount){
+        // This would be the opposite of damageValue(). Look at that for more details
+        
+        health += amount;
+
+    }
+
+    void checkASEToRemoveAtPos(ActiveSpellEffect inASE, int listPos){
+        // Remove ActiveSpellEffect is it's duration is <= 0.0f
+
+        if(inASE.getDuration() <= 0.0f){
             Debug.Log(actorName + ": Removing.. "+ inASE.getEffectName());
             activeSpellEffects.RemoveAt(listPos);
         }
     }
 
     public void applySpellEffect(SpellEffect inSpellEffect, Actor inCaster){
+
         activeSpellEffects.Add(new ActiveSpellEffect(inSpellEffect, inCaster));
+        //Debug.Log("Actor: Applying.." + inSpellEffect.getEffectName() + " to " + actorName);  
+
+    }
+    void handleDamage(ActiveSpellEffect inASE){// Type 0
+
+        /* 
+            In here you could add interesting interactions
+            Maybe something like 
+            if(ReverseDamageAndHealing)
+                then call restoreValue() instead
+        */
+        
+        damageValue( (int) inASE.getPower() );// likly will change once we have stats
+
+        // For saftey to make sure that the effect is removed from list
+        // right aft the effect finishes
+        inASE.duration = 0.0f; 
+            
+    }
+    void handleDoT(ActiveSpellEffect inASE){// Type 2
+
+        // Do any extra stuff
+
+        damageValue( (int) ( ( inASE.getTickRate() / inASE.getDuration() ) * inASE.getPower() ) );// likly will change once we have stats
+        inASE.lastTick = 0.0f;
+            
+    }
+
+    void handleHeal(ActiveSpellEffect inASE){// Type 0
+
+        /* 
+            In here you could add interesting interactions
+            Maybe something like 
+            if(ReverseDamageAndHealing)
+                then call damageValue() instead
+        */
+        
+        restoreValue( (int) inASE.getPower() ); // likly will change once we have stats
+
+        // For saftey to make sure that the effect is removed from list
+        // right aft the effect finishes
+        inASE.duration = 0.0f; 
+            
+    }
+
+    void handleHoT(ActiveSpellEffect inASE){// Type 2
+
+        // Do any extra stuff
+
+        restoreValue( (int) ( ( inASE.getTickRate() / inASE.getDuration() ) * inASE.getPower() ) );// likly will change once we have stats
+        inASE.lastTick = 0.0f;
+            
     }
     
+
+    float RoundToNearestHalf(float value)
+    {
+        return MathF.Round(value * 2) / 2;
+    }
 }
