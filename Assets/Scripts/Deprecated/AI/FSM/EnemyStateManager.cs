5b2f2c9f83359a0cebcac1b5506de188d616dd8a
Pierre Montagne <pierreb.montagne@gmail.com>
2022-09-18 17:26:18 -0400

    Deprecated FSM and old enemy controllers

---
+++ Assets/Scripts/Deprecated/AI/FSM/EnemyStateManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateManager : MonoBehaviour
+{
+    public Unit unit;
+    public EnemyBaseState currentState;
+    public string myState;
+    public PatrolState patrolState = new PatrolState();
+    public PathfindingState pathfindingState = new PathfindingState();
+    public MoveTowardsState moveTowardsState = new MoveTowardsState();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unit = GetComponent<Unit>();
+        currentState = patrolState;
+        currentState.EnterState(this);
+        myState = currentState.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.UpdateState(this);
+    }
+
+    public void SwitchState(EnemyBaseState state)
+    {
+        currentState = state;
+        state.EnterState(this);
+        myState = currentState.ToString();
+    }
+}
--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,19 +1,47 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TheKiwiCoder;
 
 public class EnemyController : MonoBehaviour
-{   
+{
     public EnemySO enemyStats;
+    // The main behaviour tree asset
+    public BehaviourTree tree;
+    public List<Ability_V2> abilities;
+
+    private Actor actor;
+
+    // Storage container object to hold game object subsystems
+    Context context;
+    public Vector3 spawnLocation;
+    private Dictionary<string, object> extra = new Dictionary<string, object>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnLocation = transform.position;
+        ExtraValues();
+        context = Context.CreateFromGameObject(gameObject, extra, enemyStats);
+        tree = tree.Clone();
+        tree.Bind(context);
+        spawnLocation = transform.position;
+        actor = gameObject.GetComponent<Actor>();
+    }
+
+    // Any extra values you want the behavior tree to have access to should be added here
+    void ExtraValues()
+    {
+        extra["spawnLocation"] = spawnLocation;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (tree)
+        {
+            tree.Update();
+        }
         /*for(i = 0; i < AbilityList.Count(); i++){
             if(AbilityList[i].CD is ready?){
                 
