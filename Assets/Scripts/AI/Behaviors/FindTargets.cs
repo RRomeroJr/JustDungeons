01af3fd9ffd6348a7a61c23ec92ac0dc1bc742e4
Pierre Montagne <pierreb.montagne@gmail.com>
2022-09-22 18:18:05 -0400

    Add 3 new behaviors: FindTargets, HasResource and SetTarget

---
+++ Assets/Scripts/AI/Behaviors/FindTargets.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+// Finds targets within a range using a raycast set to a certain layermask and by default sets target to closest
+// Random will set the target to a random one within a range
+// Will also construct a list of multiple targets within range as long as random is not set
+public class FindTargets : ActionNode
+{
+    public LayerMask targetMask;
+    public float range;
+    public bool random = false;
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        if (context.controller.FindTargets(targetMask, range, random))
+        {
+            return State.Success;
+        }
+        return State.Failure;
+    }
+}
---
+++ Assets/Scripts/AI/Behaviors/HasResource.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class HasResource : ActionNode
+{
+    public Ability_V2 ability;
+    float resourceCost;
+    // store resourcetype
+    protected override void OnStart()
+    {
+        // set resourcecost and resourcetype
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        // call checkforresource
+        return State.Success;
+    }
+}
