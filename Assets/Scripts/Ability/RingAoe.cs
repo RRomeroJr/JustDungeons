b13abefbc4355ce0967e2a20b9b5684de7dd8e75
dgb1110110 <117.rromero@gmail.com>
2022-09-24 12:15:52 -0400

    RR: Started RingAoe. safe zone doesn't work yet

--- Assets/Scripts/Ability/RingAoe.cs
+++ Assets/Scripts/Ability/RingAoe.cs
@@ -4,20 +4,27 @@ using System;
 using UnityEngine;
 using Mirror;
 [System.Serializable]
-[CreateAssetMenu(fileName="Aoe", menuName = "HBCsystem/Aoe")]
-public class Aoe : AbilityEff
+[CreateAssetMenu(fileName="RingAoe", menuName = "HBCsystem/RingAoe")]
+public class RingAoe : Aoe
 {   
-    public int school =-1;
-    public Vector3 prefabScale = Vector3.one;
-    public GameObject aoePrefab;
+    public float innerCircleRadius;
+    public Vector3 innerCircleScale;
+    public Vector3 innerCirclePosistion; //relative to parent
+    public bool usePosition = false;
+    
     public override void startEffect(Actor _target = null, NullibleVector3 _targetWP = null, Actor _caster = null){
         //Debug.Log("Actor " + _caster.getActorName() + ": casting Missile at " + _target.getActorName());
         //Debug.Log("Caster " + _caster.getActorName() + " currently has target " + _caster.target.getActorName());
-        Debug.Log(_targetWP == null ? "Aoe: No targetWP" : ("Aoe: wp = " + _targetWP.Value.ToString()));
+        Debug.Log(_targetWP == null ? "RingAoe: No targetWP" : ("RingAoe: wp = " + _targetWP.Value.ToString()));
         GameObject delivery = Instantiate(aoePrefab, getWP(_target, _targetWP), Quaternion.identity);
         delivery.GetComponent<AbilityDelivery>().setTarget(_target);
         delivery.GetComponent<AbilityDelivery>().setCaster(_caster);
         delivery.GetComponent<AbilityDelivery>().worldPointTarget = getWP(_target, _targetWP);
+        delivery.transform.localScale = prefabScale;
+        delivery.transform.GetChild(0).transform.localScale = innerCircleScale;
+        if(usePosition){
+            delivery.transform.GetChild(0).transform.position = innerCirclePosistion;
+        }
         NetworkServer.Spawn(delivery);
         
         /*
@@ -26,7 +33,7 @@ public class Aoe : AbilityEff
         */
        
     }
-    public Aoe(string _effectName, GameObject _aoePrefab, int _id = -1, float _power = 0, int _school = -1){
+    public RingAoe(string _effectName, GameObject _aoePrefab, int _id = -1, float _power = 0, int _school = -1){
         effectName = _effectName;
 
         aoePrefab = _aoePrefab;
@@ -35,28 +42,21 @@ public class Aoe : AbilityEff
         power = _power;
         school = _school;
     }
-    public Aoe(){}
+    public RingAoe(){}
     public override AbilityEff clone()
     {
-        Aoe temp_ref = ScriptableObject.CreateInstance(typeof (Aoe)) as Aoe;
+        RingAoe temp_ref = ScriptableObject.CreateInstance(typeof (RingAoe)) as RingAoe;
         temp_ref.effectName = effectName;
         temp_ref.id = id;
         temp_ref.power = power;
         temp_ref.prefabScale = prefabScale;
+        temp_ref.innerCircleRadius = innerCircleRadius;
+        temp_ref.innerCircleScale = innerCircleScale;
+        temp_ref.innerCirclePosistion = innerCirclePosistion;
+        temp_ref.usePosition = usePosition;
         temp_ref.aoePrefab = aoePrefab;
         
 
         return temp_ref;
-    }
-    public Vector3 getWP(Actor _target = null, NullibleVector3 _targetWP = null){
-        if((_targetWP == null)&&(_target != null)){
-            return _target.transform.position;
-        }
-        else if((_targetWP != null)&&(_target == null)){
-            return _targetWP.Value;
-        }
-        else{
-            throw new NullReferenceException();
-        }
     }
 }
