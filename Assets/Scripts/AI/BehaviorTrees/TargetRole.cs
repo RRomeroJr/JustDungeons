d2eadc72bfa33fb33f99e3145d5b4157f602d321
Pierre Montagne <pierreb.montagne@gmail.com>
2022-10-11 17:35:50 -0400

    TargetRole behavior created

---
+++ Assets/Scripts/AI/BehaviorTrees/TargetRole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class TargetRole : ActionNode
+{
+    public Role role;
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        if (context.controller.TargetRole(role))
+        {
+            return State.Success;
+        }
+        return State.Failure;
+    }
+}
