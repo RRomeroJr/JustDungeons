c9d23c4d4c5e27163bf0bb14ca4d386d702da8d2
Pierre Montagne <pierreb.montagne@gmail.com>
2022-10-16 20:07:31 -0400

    Title screen created

---
+++ Assets/Scripts/Menu/NetworkManagerLobby.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+using System;
+
+public class NetworkManagerLobby : NetworkManager
+{
+    [Scene][SerializeField] private string menuScene = string.Empty;
+    [Header("Room")]
+    [SerializeField] private PlayerLobby roomPlayerPrefab = null;
+
+    public static event Action OnClientConnected;
+    public static event Action OnClientDisconnected;
+
+    public override void OnStartClient()
+    {
+        var spawnablePrefabs = Resources.LoadAll<GameObject>("SpawnablePrefabs");
+
+        foreach (var prefab in spawnablePrefabs)
+        {
+            NetworkClient.RegisterPrefab(prefab);
+        }
+    }
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+
+        OnClientConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        OnClientDisconnected?.Invoke();
+    }
+
+    public override void OnServerConnect(NetworkConnectionToClient conn)
+    {
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().path != menuScene)
+        {
+            conn.Disconnect();
+            return;
+        }
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        if (SceneManager.GetActiveScene().path == menuScene)
+        {
+            PlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+
+            NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
+        }
+    }
+}
