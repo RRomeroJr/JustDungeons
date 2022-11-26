879992949b4e8318f9adfab28836454c2c399d7d
dgb1110110 <117.rromero@gmail.com>
2022-11-26 18:50:28 -0500

    RR: Mob spawner spawn mobs from list

--- Assets/Scripts/MobSpawner.cs
+++ Assets/Scripts/MobSpawner.cs
@@ -6,9 +6,10 @@ using Mirror;
 public class MobSpawner: MonoBehaviour
 {
     public GameObject mobPrefab;
+    public List<GameObject> mobPrefabList;
     void Start(){
-        StartCoroutine(tempSpawnTestMob(mobPrefab));
-        
+        //StartCoroutine(tempSpawnTestMob(mobPrefab));
+        spawnMobs();
     }
 	IEnumerator tempSpawnTestMob(GameObject prefab){
         while(true){
@@ -16,5 +17,15 @@ public class MobSpawner: MonoBehaviour
             NetworkServer.Spawn(Instantiate(prefab, transform.position, Quaternion.identity));
 
         }
-    }       
+    }
+    void spawnMobs(){
+            
+        for (int i = 0; i < mobPrefabList.Count; i++)
+        {
+            GameObject goRef = Instantiate(mobPrefabList[i], transform.position, Quaternion.identity);
+            goRef.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f,0.01f));
+            NetworkServer.Spawn(goRef);
+            //goRef.GetComponent<Controller>().moveOffOtherUnits();
+        }
+    }
 }
