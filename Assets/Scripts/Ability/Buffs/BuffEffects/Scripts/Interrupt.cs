92dac196391da9aa727d0526ce81027f7c312550
Pierre Montagne <pierreb.montagne@gmail.com>
2023-01-01 15:14:00 -0500

    Interrupt BuffEffect and interface created

---
+++ Assets/Scripts/Ability/Buffs/BuffEffects/Scripts/Interrupt.cs
@@ -0,0 +1,15 @@
+﻿namespace BuffSystem
+{
+    public class Interrupt : BuffEffect
+    {
+        public override void ApplyEffect(IBuff t)
+        {
+            var target = t as IInterrupt;
+            if (target != null)
+            {
+                target.Interrupt();
+            }
+        }
+    }
+}
+
