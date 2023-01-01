07dfb305eace0125a4b5eec6062bda2487c30f98
Pierre Montagne <pierreb.montagne@gmail.com>
2023-01-01 15:12:30 -0500

    Fear BuffEffect and interface created

---
+++ Assets/Scripts/Ability/Buffs/BuffEffects/Interfaces/IFear.cs
@@ -0,0 +1,4 @@
+﻿public interface IFear : IBuff
+{
+    public int Feared { get; set; }
+}
---
+++ Assets/Scripts/Ability/Buffs/BuffEffects/Scripts/Fear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace BuffSystem
+{
+    [CreateAssetMenu(fileName = "Assets/Scripts/Ability/Buffs/BuffEffects/ScriptableObjects/NewFearEffect", menuName = "HBCsystem/Buffs/Fear")]
+    public class Fear : BuffEffect
+    {
+        public override void EndEffect(IBuff t)
+        {
+            var target = t as IFear;
+            if (target != null)
+            {
+                target.Feared--;
+            }
+        }
+
+        public override void StartEffect(IBuff t)
+        {
+            var target = t as IFear;
+            if (target != null)
+            {
+                target.Feared++;
+            }
+        }
+    }
+}
