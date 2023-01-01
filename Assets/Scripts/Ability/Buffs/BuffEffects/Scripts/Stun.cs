b4c32716f34adfb1ca72c4317a24661c0eadb1a1
Pierre Montagne <pierreb.montagne@gmail.com>
2023-01-01 15:11:48 -0500

    Stun BuffEffect and interface created

---
+++ Assets/Scripts/Ability/Buffs/BuffEffects/Scripts/Stun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Assets/Scripts/Ability/Buffs/BuffEffects/ScriptableObjects/NewStunEffect", menuName = "HBCsystem/Buffs/Stun")]
+public class Stun : BuffEffect
+{
+    public override void EndEffect(IBuff t)
+    {
+        var target = t as IStun;
+        if (target != null)
+        {
+            target.Stunned--;
+        }
+    }
+
+    public override void StartEffect(IBuff t)
+    {
+        var target = t as IStun;
+        if (target != null)
+        {
+            target.Stunned++;
+        }
+    }
+}
