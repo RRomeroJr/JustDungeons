db7b14e6bf12a588cbe9e56c255e5a3fb94fe3f8
Pierre Montagne <pierreb.montagne@gmail.com>
2023-01-22 11:01:37 -0500

    Dizzy Effect created

---
+++ Assets/Scripts/Ability/Buffs/BuffEffects/Scripts/Dizzy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BuffSystem
+{
+    [CreateAssetMenu(fileName = ProjectPaths.buffEffects + "NewDizzyEffect", menuName = ProjectPaths.buffEffectsMenu + "Dizzy")]
+    public class Dizzy : BuffEffect
+    {
+        public override void EndEffect(GameObject target, float effectValue)
+        {
+            if (target.TryGetComponent(out IDizzy t))
+            {
+                t.Dizzy--;
+            }
+        }
+
+        public override void StartEffect(GameObject target, float effectValue)
+        {
+            if (target.TryGetComponent(out IDizzy t))
+            {
+                t.Dizzy++;
+            }
+        }
+    }
+}
+
