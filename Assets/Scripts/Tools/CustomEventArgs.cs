5f03fd47f9a4613e37178112b5200033d72e154a
Pierre Montagne <pierreb.montagne@gmail.com>
2023-02-18 21:57:05 -0500

    Method to convert StatusEffectChangedEventArgs to a dictionary

--- Assets/Scripts/Tools/CustomEventArgs.cs
+++ Assets/Scripts/Tools/CustomEventArgs.cs
@@ -17,4 +17,16 @@ public class StatusEffectChangedEventArgs : EventArgs
     public int Stunned { get; set; }
     public int Dizzy { get; set; }
     public StatusEffectState NewEffect { get; set; }
+
+    public Dictionary<StatusEffectState, int> ToDictionary()
+    {
+        var dict = new Dictionary<StatusEffectState, int>
+        {
+            { StatusEffectState.Feared, Feared },
+            { StatusEffectState.Silenced, Silenced },
+            { StatusEffectState.Stunned, Stunned },
+            { StatusEffectState.Dizzy, Dizzy },
+        };
+        return dict;
+    }
 }
