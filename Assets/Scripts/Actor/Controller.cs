b4f41bdfa2b42b602d68da94ea7f0bf1b2312fc2
Pierre Montagne <pierreb.montagne@gmail.com>
2023-02-19 23:42:35 -0500

    Controller refactored

--- Assets/Scripts/Actor/Controller.cs
+++ Assets/Scripts/Actor/Controller.cs
@@ -1,57 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Mirror;
+using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-
 public class Controller : NetworkBehaviour
-{   [Header("For Now Needs To Be Assigned")]
+{
+    [Header("For Now Needs To Be Assigned")]
     public Ability_V2 autoAttackClone;
-    
-    float tempspeed = 0.15f;
+
     [Header("Automatic")]
     public Actor actor;
-    public NavMeshAgent agent;
-    public bool autoAttacking;
-    
     public GameObject followTarget;
-    public bool resolvingMoveTo;
-    public Vector2 facingDirection;
-     public float globalCooldown = 0.0f;
+    public float globalCooldown = 0.0f;
     public const float gcdBase = 2.0f;
-    [SyncVar]
-    public bool autoAttackRequest = false;
-    public float moveSpeed = 420;
     public List<Ability_V2> abilities;
-    [SyncVar]
-    public bool tryingToMove = false;
-    public Vector2? moveDirection;
-    public UnityEvent<bool> moveToEvent = new UnityEvent<bool>(); 
 
-    protected Rigidbody2D rb2d;
+    // Movement
+    [SyncVar]
+    public float moveSpeed = 420;
+    [SyncVar]
+    public Vector2? moveDirection;
+    public Vector2 facingDirection;
+
+    // Unity Components
+    private Rigidbody2D rb2d;
+    protected NavMeshAgent agent;
+
+    // State Values
     public bool holdDirection = false;
-    
-    public virtual void Awake(){
-        
+    public bool tryingToMove = false;
+    public bool autoAttacking;
+    public bool resolvingMoveTo;
+    public bool autoAttackRequest = false;
+
+    public virtual void Awake()
+    {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
         actor = GetComponent<Actor>();
         rb2d = GetComponent<Rigidbody2D>();
     }
-    public virtual void Start(){
+
+    public virtual void Start()
+    {
         //autoAttackClone = AbilityData.instance.AutoAttack.clone();
-        facingDirection = new Vector2(0.5f,-0.5f);
-    }
-    
-    public void MoveTowards(Vector3 pos){
-        
-        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, pos, tempspeed);
+        facingDirection = new Vector2(0.5f, -0.5f);
     }
 
-    public void MoveInDirection(Vector2 _vect){ //vector with magnitude from 0 to 1 based on input 
+    public virtual void Update()
+    {
+        if (tag == "Player")
+        {
+            Debug.DrawLine(transform.position, (facingDirection * 2.5f) + (Vector2)transform.position, Color.green);
+        }
+        if (globalCooldown > 0.0f)
+        {
+            globalCooldown -= Time.deltaTime;
+        }
+        if (autoAttacking && actor.target != null)
+        {
+            if (HBCTools.areHostle(actor, actor.target) == false)
+            {
+                autoAttacking = false;
+                return;
+            }
+            if (actor.checkOnCooldown(autoAttackClone) == false)
+            {
+                if (autoAttackRequest == false)
+                {
+                    //request aa commad?
+                    if (isServer)
+                    {
+                        handleAutoAttackRequest();
+                    }
+                    else
+                    {
+                        requestAutoAttack();
+                    }
+                }
+            }
+        }
+        if (isServer)
+        {
+            if (autoAttackRequest && actor.checkOnCooldown(autoAttackClone) == false)
+            {
+                autoAttackRequest = false;
+            }
+            if (!resolvingMoveTo && !holdDirection && followTarget != null)
+            {
+                // if(!HBCTools.checkFacing(actor, followTarget)){
+                //     facingDirection = HBCTools.ToNearest45(followTarget.transform.position - transform.position);
+                // }
+                GetComponent<NavMeshAgent>().SetDestination(followTarget.transform.position);
+            }
+        }
+        else if (rb2d.velocity.magnitude > 0.0f)
+        {
+            facingDirection = HBCTools.ToNearest45(rb2d.velocity);
+        }
+    }
+
+    public void MoveTowards(Vector3 pos)
+    {
+        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, pos, moveSpeed);
+    }
+
+    public void MoveInDirection(Vector2 _vect)
+    { //vector with magnitude from 0 to 1 based on input 
         // _vect = (moveSpeed * Time.deltaTime) * _vect;
 
         // _vect.x *= (moveSpeed * Time.fixedDeltaTime);  //speed is just d is a distance in NavMeshAgent
@@ -63,89 +121,34 @@ public class Controller : NetworkBehaviour
         _vect = moveSpeed * _vect;
         gameObject.GetComponent<Rigidbody2D>().velocity = _vect;
     }
-    
-    public virtual void Update(){
-        if(tag == "Player"){
-            Debug.DrawLine(transform.position, (facingDirection * 2.5f) + (Vector2)transform.position, Color.green);
-        } 
-        
-        if(globalCooldown > 0.0f){
-            globalCooldown -= Time.deltaTime;
-        }
-        if(autoAttacking){
-            if(actor.target != null){
-                if(HBCTools.areHostle(actor, actor.target) == false){
-                    autoAttacking = false;
-                    return;
-                }
-                if(actor.checkOnCooldown(autoAttackClone) == false){
-                    if(autoAttackRequest == false){
-                        //request aa commad?
-                        if(isServer){
-                            handleAutoAttackRequest();
-                        }
-                        else{
-                            requestAutoAttack();
-                        }
-                    }
-                }
-            }
- 
-        }
-        if(isServer){
-            if(autoAttackRequest == true){
-                    if(actor.checkOnCooldown(autoAttackClone) == false){
-                        autoAttackRequest = false;
-                    
-                    }
-          
-            }
-            if(!resolvingMoveTo && !holdDirection){
-                if(followTarget != null){
-                    // if(!HBCTools.checkFacing(actor, followTarget)){
-                    //     facingDirection = HBCTools.ToNearest45(followTarget.transform.position - transform.position);
-                    // }
-                    GetComponent<NavMeshAgent>().SetDestination(followTarget.transform.position);
-                }
-            }
-        }
-        
-        
-        else
-        {
-            if(rb2d.velocity.magnitude > 0.0f);
-            {
-                facingDirection = HBCTools.ToNearest45(rb2d.velocity);
-            }
-            
-        }
-        
-    }
+
     [Command]
-    void requestAutoAttack(){
-        
+    void requestAutoAttack()
+    {
         handleAutoAttackRequest();
     }
-    void handleAutoAttackRequest(){
-        if(autoAttackRequest == true){
+    void handleAutoAttackRequest()
+    {
+        if (autoAttackRequest == true)
+        {
             return;
         }
-        else if(actor.checkOnCooldown(autoAttackClone) == false){
-            if(autoAttackRequest == false){
-                //request aa commad?
+        if (actor.checkOnCooldown(autoAttackClone) == false)
+        {
+            //request aa commad?
 
-                autoAttackRequest = actor.castAbility3(autoAttackClone); //eventually the req becomes true
-               
-            }
-            
+            autoAttackRequest = actor.castAbility3(autoAttackClone); //eventually the req becomes true
         }
     }
     [Command]
-    public void CmdSetTryingToMove(bool _valFromClient){
+    public void CmdSetTryingToMove(bool _valFromClient)
+    {
         tryingToMove = _valFromClient;
     }
-    public bool moveToPoint(Vector2 pos){
-        if(resolvingMoveTo){
+    public bool moveToPoint(Vector2 pos)
+    {
+        if (resolvingMoveTo)
+        {
             return false;
         }
         StartCoroutine(IE_moveToPoint(pos));
@@ -158,33 +161,37 @@ public class Controller : NetworkBehaviour
     //     StartCoroutine(IE_moveToPoint(pos, tempMoveSpeed));
     //     return true;
     // }
-    public void moveOffOtherUnits(){
+    public void moveOffOtherUnits()
+    {
         moveToPoint(Vector2.up + (Vector2)transform.position);
     }
-    IEnumerator IE_moveToPoint(Vector2 pos){
+    IEnumerator IE_moveToPoint(Vector2 pos)
+    {
         /*
             I have an idea here. Make this method hold the target like the node does
             then set agent.destination to pos and check to see if it arrived every 0.2s
             or so 
         */
-        if(resolvingMoveTo){
+        if (resolvingMoveTo)
+        {
             Debug.Log("already resolving moving finishing early");
             agent.SetDestination(pos);
             yield return new WaitForSeconds(0.0f);
-            
         }
-        else{
-
+        else
+        {
             //Debug.Log("No Pending Path move to: " + pos);
             agent.ResetPath();
             agent.SetDestination(pos);
-            
+
             agent.stoppingDistance = 0;
             resolvingMoveTo = true;
             agent.isStopped = false;
-            
-            while((Vector2.Distance(pos, transform.position) > 0.1f)){
-                if(!agent.hasPath && !agent.pathPending){
+
+            while ((Vector2.Distance(pos, transform.position) > 0.1f))
+            {
+                if (!agent.hasPath && !agent.pathPending)
+                {
                     agent.SetDestination(pos);
                 }
                 yield return new WaitForSeconds(0.02f);
@@ -193,14 +200,11 @@ public class Controller : NetworkBehaviour
             agent.ResetPath();
             //Debug.Log("Move To Finshed. Distance: " + Vector2.Distance(pos, gameObject.transform.position).ToString() + "Stopping distance: " + agent.stoppingDistance );
             resolvingMoveTo = false;
-            if(followTarget != null){
+            if (followTarget != null)
+            {
                 agent.stoppingDistance = getStoppingDistance(followTarget);
             }
-            
         }
-        
-        
-        
     }
     //  IEnumerator IE_moveToPoint(Vector2 pos, float tempMoveSpeed){
     //     float moveSpeedHolder = agent.speed;
@@ -211,35 +215,35 @@ public class Controller : NetworkBehaviour
     //     }
     //     Debug.Log(actor.getActorName()+": Returning normal agent speed");
     //     agent.speed = moveSpeedHolder;
-        
+
     // }
-  
-    float getStoppingDistance(GameObject _target){
+
+    float getStoppingDistance(GameObject _target)
+    {
         float selfDiagonal;
         float tragetDiagonal;
         selfDiagonal = Mathf.Sqrt(Mathf.Pow(gameObject.GetComponent<Renderer>().bounds.size.x, 2)
                             + Mathf.Pow(gameObject.GetComponent<Renderer>().bounds.size.x, 2));
         tragetDiagonal = Mathf.Sqrt(Mathf.Pow(followTarget.GetComponent<Collider2D>().bounds.size.x, 2)
                             + Mathf.Pow(followTarget.GetComponent<Collider2D>().bounds.size.x, 2));
-        return ((tragetDiagonal + selfDiagonal) /2) * 0.9f;
-
-                            
+        return ((tragetDiagonal + selfDiagonal) / 2) * 0.9f;
     }
-    public void SetFollowTarget(GameObject _target){
+    public void SetFollowTarget(GameObject _target)
+    {
         followTarget = _target;
-        if(followTarget != null){
+        if (followTarget != null)
+        {
             agent.stoppingDistance = getStoppingDistance(followTarget);
         }
-        
     }
     [Command]
-    protected void CmdSetFacingDirection(Vector2 _ClientFacingDirection){
+    protected void CmdSetFacingDirection(Vector2 _ClientFacingDirection)
+    {
         RpcSetFacingDirection(_ClientFacingDirection);
     }
     [ClientRpc(includeOwner = false)]
-    protected void RpcSetFacingDirection(Vector2 _ownersFacingDirection){
+    protected void RpcSetFacingDirection(Vector2 _ownersFacingDirection)
+    {
         facingDirection = _ownersFacingDirection;
     }
-    
-    
 }
