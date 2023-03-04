08f2dcc9890b7be54e59dfb10e86a49179dab140
Pierre Montagne <pierreb.montagne@gmail.com>
2023-03-04 10:46:24 -0500

    Each buff bar item is now in charge of updating its UI itself rather the parent BuffBar script

---
+++ Assets/Scripts/UI/BuffBar/BuffBarItemUI.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using BuffSystem;
+
+public class BuffBarItemUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI buffName;
+    [SerializeField] private TextMeshProUGUI remainingTime;
+    public Buff Buff { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Buff.RemainingBuffTime / Buff.BuffSO.Duration;
+        remainingTime.text = Buff.RemainingBuffTime.ToString("0.0");
+    }
+
+    public void AssignBuff(Buff b)
+    {
+        Buff = b;
+        image.sprite = Buff.BuffSO.Icon;
+        buffName.text = Buff.BuffSO.name;
+    }
+
+    private void OnDisable()
+    {
+        Buff = null;
+    }
+}
