c3f828e0d80ac3ac367899ba8465d2f7d380b823
dgb1110110 <117.rromero@gmail.com>
2023-06-23 14:10:14 -0400

    Listener added in Start instead of Awake

--- Assets/Scripts/Ability/Scripts/CombatEndDespawn.cs
+++ Assets/Scripts/Ability/Scripts/CombatEndDespawn.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class CombatEndDespawn: NetworkBehaviour
+public class CombatEndDespawn : NetworkBehaviour
 {
-	void Awake()
+	void Start()
     {
         if(isServer){
+            // Debug.Log("AddListener");
             GameManager.instance.AllPlayersLeaveCombat.AddListener(DespawnObject);
         }
+        // else
+        // {
+        //     Debug.Log("AddListener SKIPPED");
+        // }
+
 
     }
     [Server]
     void DespawnObject()
     {
+        // Debug.Log("DespawnObject");
+        GameManager.instance.AllPlayersLeaveCombat.RemoveListener(DespawnObject);
         Destroy(gameObject);
+
+
     }
 }
