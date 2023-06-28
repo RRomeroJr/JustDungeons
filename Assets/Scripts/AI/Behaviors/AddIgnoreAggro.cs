29c1be0c9dea9aeffa0d0e8119ac8200bb1de0db
dgb1110110 <117.rromero@gmail.com>
2023-06-27 21:55:27 -0400

    DioMiniBoss

---
+++ Assets/Scripts/AI/Behaviors/AddIgnoreAggro.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheKiwiCoder;
+
+public class AddIgnoreAggro : ActionNode
+{
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        (context.controller as EnemyController).tauntImmune += 1;
+        return State.Success;
+        
+    }
+}
