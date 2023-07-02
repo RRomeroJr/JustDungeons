5577ca44f4e06c27f9148a00f6c5bb175691e0e5
dgb1110110 <117.rromero@gmail.com>
2023-07-02 16:47:37 -0400

    ClassStats. health rm. MainStat + HealingPower added

--- Assets/Scripts/Ability/Class/ClassStats.cs
+++ Assets/Scripts/Ability/Class/ClassStats.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName="ClassStats", menuName = "HBCsystem/Classes/ClassStats")]
 public class ClassStats: ScriptableObject
 {
-	public int health;
+	// public int health;
     public float healthMutliplier = 1.0f;
+    public float mainStat = 100.0f;
+    public float HealingPower = 10.0f;
     
 }
