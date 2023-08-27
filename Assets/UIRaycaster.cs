b8ce0917a4b80ea39200df908d457bbc407613b2
dgb1110110 <117.rromero@gmail.com>
2023-08-27 01:39:33 -0400

    Raycasting logic moved to meth. Update logic commented out

--- Assets/UIRaycaster.cs
+++ Assets/UIRaycaster.cs
@@ -27,29 +27,41 @@ public class UIRaycaster : MonoBehaviour
     void Update()
     {
         //Check if the left Mouse button is clicked
-        if (Input.GetKey(KeyCode.Mouse0))
+        // if (Input.GetKey(KeyCode.Mouse0))
+        // {
+        //     UnitFrame frame = GraphicsRaycastToUnitFrame();
+        //     if(frame)
+        //     {
+        //         OnUIFrameClicked(frame);
+        //     } 
+        // }
+    }
+    /// <summary>
+    /// Look for UnitFrame at current mouse posistion
+    /// </summary>
+    public UnitFrame GraphicsRaycastToUnitFrame()
+    {
+        //Set up the new Pointer Event
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        //Set the Pointer Event Position to that of the mouse position
+        m_PointerEventData.position = Input.mousePosition;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        foreach (RaycastResult result in results)
         {
-            //Set up the new Pointer Event
-            m_PointerEventData = new PointerEventData(m_EventSystem);
-            //Set the Pointer Event Position to that of the mouse position
-            m_PointerEventData.position = Input.mousePosition;
-
-            //Create a list of Raycast Results
-            List<RaycastResult> results = new List<RaycastResult>();
-
-            //Raycast using the Graphics Raycaster and mouse click position
-            m_Raycaster.Raycast(m_PointerEventData, results);
-
-            foreach (RaycastResult result in results)
+            UnitFrame frame = result.gameObject.GetComponent<UnitFrame>();
+            if (frame)
             {
-                UnitFrame frame = result.gameObject.GetComponent<UnitFrame>();
-                if (frame)
-                {
-                    OnUIFrameClicked(frame);
-                    break;
-                }
+                return frame;
             }
         }
+
+        return null;
     }
 
     protected virtual void OnUIFrameClicked(UnitFrame frame)
