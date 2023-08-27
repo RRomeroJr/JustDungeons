a96d3a74940e584d1a0800f19f6db85bbf1be5a3
dgb1110110 <117.rromero@gmail.com>
2023-08-27 01:38:05 -0400

    ClickManager overhaul. To be used for long/ short clicks

--- Assets/Scripts/UI/ClickManager.cs
+++ Assets/Scripts/UI/ClickManager.cs
@@ -5,109 +5,102 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Mirror;
 using System;
-/*
-Richie: 
-This should handle clicking of characters/ objects in the world
-as well as UI elements inorder to interact with them/ get them
-as your current target
-*/
+
+
 public class ClickManager : NetworkBehaviour
 {
-    /*
-        Note:
-                RR: This relies on a GameObejct existing with the Unity tag "MainCamera" and there
-                    being only one of them
-    */
+    public float clickWindow = 0.66f;
+    public float clickTravelWindow = 66.0f;
+    public ClickData clickData0 = new ClickData();
+    public ClickData clickData1 = new ClickData();
+    public LayerMask targetingLayerMask;
 
-
-
-    public Text targetName;
-    public Slider targetHealthBar;
-    public Image targetHealthBarFill;
-    public Actor playerActor;
-    public LayerMask mask;
-
-    void Start()
+    public void Awake()
     {
-        FindObjectOfType<UIRaycaster>().UIFrameClicked += OnUIFrameClicked;
+        targetingLayerMask = LayerMask.GetMask("UI");
     }
-
-    void Update()
-    {   
-        if(isLocalPlayer){
-            if (Input.GetMouseButtonDown(0)) {
-                /*
-                        Implement Clicking UI frames to get target somehow 
-                */
-
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, mask);
-                //Debug.Log("mousePos "+ mousePos.ToString());
-
-                if (hit.collider != null) {
-                    
-                    Debug.Log("Clicked something: " + hit.collider.gameObject.name);
-
-                    // set controller's target w/ actor hit by raycast
-
-                    if(isServer){
-                        playerActor.rpcSetTarget(hit.collider.gameObject.GetComponent<Actor>());
-                    }else{
-                        playerActor.cmdReqSetTarget(hit.collider.gameObject.GetComponent<Actor>());
-                    }
-                }else{
-                    //Debug.Log("Nothing clicked");
-                }
-            }
-            if (Input.GetMouseButtonDown(1)) {
-                /*
-                        Implement Clicking UI frames to get target somehow 
-                */
-
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, mask);
-                //Debug.Log("mousePos "+ mousePos.ToString());
-
-                if (hit.collider != null) {
-                    
-                    Debug.Log("Clicked something: " + hit.collider.gameObject.name);
-
-                    // set controller's target w/ actor hit by raycast
-                    playerActor.GetComponent<Controller>().autoAttacking = true;
-                    if(isServer){
-                        playerActor.rpcSetTarget(hit.collider.gameObject.GetComponent<Actor>());
-                    }else{
-                        playerActor.cmdReqSetTarget(hit.collider.gameObject.GetComponent<Actor>());
-                    }
-                    
-                }else{
-                    
-                    playerActor.GetComponent<Controller>().autoAttacking = false;
-                }
-            }
-        }
-    }
-
-    private void OnUIFrameClicked(object source, UIFrameClickedEventArgs e)
+    public void Update()
     {
-        if (isServer)
+        if (Input.GetMouseButtonDown(0))
         {
-            playerActor.rpcSetTarget(e.Frame.actor);
+            clickData0.ClickStart();
         }
-        else
+        if (Input.GetMouseButtonDown(1))
         {
-            playerActor.cmdReqSetTarget(e.Frame.actor);
+            clickData1.ClickStart();
         }
+    }
+    /// <summary>
+    ///	If the mouse button is held and mouse posistion moved atleast the clickTravelWindow distance
+    /// </summary>
+    public bool MouseButtonDrag(int _buttonId)
+    {
+        switch (_buttonId)
+        {
+            case (0):
+                return (Input.GetMouseButton(0) && clickData0.CalcTravel() >= clickTravelWindow);
+                break;
+            case (1):
+                return (Input.GetMouseButton(1) && clickData1.CalcTravel() >= clickTravelWindow);
+                break;
+            default:
+                Debug.LogError("Unknown mouse button for click");
+                break;
+        }
+        Debug.Log("CM drag: " + false);
+        return false;
+    }
+    /// <summary>
+    ///	If the mouse button was held for less than or equal to the clickWindow
+    /// </summary>
+    public bool MouseButtonShort(int _buttonId)
+    {
+        switch (_buttonId)
+        {
+            case (0):
+                return clickData0.CalcHoldTime() <= clickWindow;
+                break;
+            case (1):
+                return clickData1.CalcHoldTime() <= clickWindow;
+                break;
+            default:
+                Debug.LogError("Unknown mouse button for click");
+                break;
+        }
+        Debug.Log("CM short: " + false);
+        return false;
+    }
+    public Vector2 MouseButtonDragVector(int _buttonId)
+    {
+        switch (_buttonId)
+        {
+            case (0):
+                return Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.ScreenToWorldPoint(clickData0.startPos);
+                break;
+            case (1):
+                return Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.ScreenToWorldPoint(clickData1.startPos);
+                break;
+            default:
+                Debug.LogError("Unknown mouse button for Drag vector");
+                break;
+        }
+        return Vector2.zero;
+    }
+    public bool MouseButtonHold(int _buttonId)
+    {
+        switch (_buttonId)
+        {
+            case (0):
+                return (Input.GetMouseButton(0) && clickData0.CalcHoldTime() <= clickWindow);
+                break;
+            case (1):
+                return (Input.GetMouseButton(1) && clickData1.CalcHoldTime() <= clickWindow);
+                break;
+            default:
+                Debug.LogError("Unknown mouse button for click");
+                break;
+        }
+        return false;
     }
 
-    [ClientRpc]
-    void updateTargetToClients(Actor target){
-        playerActor.target = target;
-    }
-    [Command]
-    void reqTargetUpdate(Actor _actor){ //in future this should be some sort of act id or something
-        updateTargetToClients(_actor);
-    }
 }
