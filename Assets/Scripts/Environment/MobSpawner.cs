9195dc4617649912d8805676c7e61aa90e3cc483
dgb1110110 <117.rromero@gmail.com>
2023-12-02 10:49:24 -0500

    Updates to spawners, Doors for boss exits, event proof of concept for aft b1 & b2

--- Assets/Scripts/Environment/MobSpawner.cs
+++ Assets/Scripts/Environment/MobSpawner.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Mirror;
+using System.Threading;
 
 public class MobSpawner: MonoBehaviour
 {
     // public GameObject mobPrefab;
     public List<GameObject> mobPrefabList;
     public bool spawnOnStart = true;
+    public UnityEvent<List<Actor>> SpawnListOut = new UnityEvent<List<Actor>>(); 
     void Start(){
         //StartCoroutine(tempSpawnTestMob(mobPrefab));
         if(NetworkServer.active && spawnOnStart){
@@ -25,14 +28,17 @@ public class MobSpawner: MonoBehaviour
         }
     }
     public void spawnMobs(){
-            
+        List<Actor> spawnList = new List<Actor>();
         for (int i = 0; i < mobPrefabList.Count; i++)
         {
             GameObject goRef = Instantiate(mobPrefabList[i], transform.position, Quaternion.identity);
             goRef.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f,0.01f));
             goRef.AddComponent<GeneralMobPack>();
+            spawnList.Add(goRef.GetComponent<Actor>());
             NetworkServer.Spawn(goRef);
             //goRef.GetComponent<Controller>().moveOffOtherUnits();
         }
+        
+        SpawnListOut?.Invoke(spawnList);
     }
 }
--- Assets/Scripts/Triggers/MobDeathTrigger.cs
+++ Assets/Scripts/Triggers/MobDeathTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,6 +8,7 @@ public class MobDeathTrigger: Trigger
 {
 	
     public List<Actor> actorsTocheck;
+    public bool waitToPopulate = false; 
     
     void Awake()
     {
@@ -29,17 +31,28 @@ public class MobDeathTrigger: Trigger
 
         foreach(Actor actorToCheck in actorsTocheck)
         {
-            if(actorToCheck != null){
-
-                atleastOneAlive = actorToCheck.state == ActorState.Alive;
+            if(waitToPopulate){ // There was atleast 1 actor in the list so no longer waiting to populate
+                waitToPopulate = false;
             }
-
-            if(atleastOneAlive)
-                break;
+            if(actorToCheck != null){
+                if(actorToCheck.state == ActorState.Alive)
+                {
+                    atleastOneAlive = true;
+                    break;
+                }
+            }
             
         }
+       
+        return !(atleastOneAlive || waitToPopulate);
+    }
 
-        return !atleastOneAlive;
+    public void PopulateActorList(List<Actor> _list)
+    {
+        foreach(Actor a in _list)
+        {
+            actorsTocheck.Add(a);
+        }
     }
    
 }
