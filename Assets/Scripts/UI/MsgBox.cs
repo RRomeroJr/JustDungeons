3b7f72d9e10a5e1a268cd261ef383b612f8e11cd
dgb1110110 <117.rromero@gmail.com>
2024-02-15 14:20:27 -0500

    MsgBox for displaying stuff to the player

---
+++ Assets/Scripts/UI/MsgBox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class MsgBox : MonoBehaviour
+{
+  public List<MsgBoxItem> mbiList = new List<MsgBoxItem>();
+  public static MsgBox instance;
+
+  void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+  public static void DisplayMsg(string _in)
+  {
+    for(int i = instance.mbiList.Count - 1; i > 0; i--)
+    {
+    if(instance.mbiList[i - 1].gameObject.activeSelf == false)
+    {// if the one above is not active, skip
+      continue;
+    }
+
+    instance.mbiList[i].StartDisplaying(instance.mbiList[i - 1].tmpUi.text, instance.mbiList[i - 1].timer);
+    
+    }
+
+
+    instance.mbiList[0].StartDisplaying(_in);
+
+  }
+}
---
+++ Assets/Scripts/UI/MsgBoxItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+
+public class MsgBoxItem : MonoBehaviour
+{
+  const float CLEARTIMER = 5.0f;
+  public TextMeshProUGUI tmpUi;
+  public float timer = 0.0f;
+  void Awake()
+  {
+    tmpUi = GetComponent<TextMeshProUGUI>();
+  }
+  void FixedUpdate()
+  {
+    if(timer > 0)
+    {
+      timer -= Time.fixedDeltaTime;
+    }
+    if(timer < 0)
+    {
+      // tmpUi.text = "";
+      gameObject.SetActive(false);
+    }
+  }
+  /// <summary>
+  /// Sets text and restarts timer
+  /// </summary>
+  /// <param name="_in"></param>
+  public void StartDisplaying(string _in, float _time = CLEARTIMER){
+    if(!gameObject.activeSelf)
+    {
+      gameObject.SetActive(true);
+    }
+    tmpUi.text = _in;
+    timer = _time;
+  }
+}
