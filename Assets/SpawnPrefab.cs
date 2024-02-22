c3f9985c04b39074cc4ed606a781c440b1ed91d5
Pierre Montagne <pierreb.montagne@gmail.com>
2024-02-22 18:49:11 -0500

    New behavior to spawn prefabs

---
+++ Assets/SpawnPrefab.cs
@@ -0,0 +1,30 @@
+﻿using Mirror;
+using TheKiwiCoder;
+using UnityEngine;
+
+public class SpawnPrefab : ActionNode
+{
+    public GameObject prefab;
+    public Vector2 relativePosition;
+    public string name;
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        GameObject spawnedObject = Instantiate(prefab, context.transform.position + (Vector3)relativePosition, Quaternion.identity);
+        spawnedObject.name = name;
+        if (spawnedObject == null)
+        {
+            return State.Failure;
+        }
+        NetworkServer.Spawn(spawnedObject);
+        return State.Success;
+    }
+}
