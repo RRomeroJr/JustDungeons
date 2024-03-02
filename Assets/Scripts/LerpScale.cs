98979d18d962e4e65640e6e94bde265122bc90e4
Pierre Montagne <pierreb.montagne@gmail.com>
2024-03-02 16:49:49 -0500

    New script to lerp the scale of a game object

---
+++ Assets/Scripts/LerpScale.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LerpScale : MonoBehaviour
+{
+    [SerializeField] private Vector3 startScale;
+    [SerializeField] private Vector3 endScale;
+    [SerializeField] private float duration;
+
+    private float endTime;
+
+    void Start()
+    {
+        endScale = transform.localScale;
+        transform.localScale = startScale;
+        endTime = Time.time + duration;
+    }
+
+    void Update()
+    {
+        float t = (endTime - Time.time) / duration;
+        transform.localScale = Vector3.Lerp(endScale, startScale, t);
+    }
+}
