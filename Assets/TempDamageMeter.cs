8bb28386c973eec7f543e8af1da4b8e6b7b19e5b
dgb1110110 <117.rromero@gmail.com>
2024-03-18 20:11:45 -0400

    Dmg meter returns and only tracks while in combat

--- Assets/TempDamageMeter.cs
+++ Assets/TempDamageMeter.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Mirror;
 
-public class TempDamageMeter : MonoBehaviour
+public class TempDamageMeter : NetworkBehaviour
 {
     // Use Uimanager to reference the player
     // interate over player's cooldowns
     // display them in the textmeshpro.SetText
     public TextMeshProUGUI tmpGUI;
     public List<MeterEntry> entryList;
-    public float timePassed = 0.01f;
+    
+    public float timePassed = 0.001f;
+    public float updateMax = 1.0f;
+    float updateTimer = 0.0f;
     public static TempDamageMeter instance;
+    [SyncVar]
+    /// <summary>
+    /// True if atleast 1 player in combat
+    /// </summary>
+    public bool playerCombat = false;
 
     void Awake()
     {
@@ -35,26 +44,42 @@ public class TempDamageMeter : MonoBehaviour
     {
         tmpGUI = GetComponent<TextMeshProUGUI>();
         entryList = new List<MeterEntry>();
+        if(isServer)
+        {
+            GameManager.instance.OnActorEnterCombat.AddListener(OnActorEnterCombat);
+            GameManager.instance.AllPlayersLeaveCombat.AddListener(AllPlayersLeaveCombat);
+            playerCombat = GameManager.PlayersInCombat();
+        }
+        updateTimer = updateMax;
+    }
+    void OnActorEnterCombat(Actor _a)
+    {
+        if(_a.tag == "Player")
+        {
+            playerCombat = true;
+        }
+    }
+    void AllPlayersLeaveCombat()
+    {
+        playerCombat = false;
+        DisplayEntries();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        string toDisplay = "";
-        if(entryList.Count > 0){
-            
-            foreach(MeterEntry me in entryList){
-                //Debug.Log("Displaying cooldowns");
-                toDisplay = toDisplay + me.actor.ActorName+ "| " + ((int)((float)me.total / timePassed)).ToString() + "\n";
-                tmpGUI.SetText(toDisplay);
+        if(playerCombat)
+        {
+            if(updateTimer >= updateMax)
+            {
+                DisplayEntries();
+                updateTimer = 0.0f;
             }
+            timePassed += Time.deltaTime;
+            updateTimer += Time.deltaTime;
         }
-        else{
-            tmpGUI.SetText(toDisplay);
-        }
-        timePassed += Time.deltaTime;
     }
+
     public static void addToEntry(Actor _actor, int _amount){
         if(!instance || !instance.enabled)
         {
@@ -72,5 +97,23 @@ public class TempDamageMeter : MonoBehaviour
             meRef.total += _amount;
             instance.entryList.Add(meRef);
         }
+        
     }
+
+    void DisplayEntries()
+    {
+        string toDisplay = "";
+        if(entryList.Count > 0){
+            
+            foreach(MeterEntry me in entryList){
+                //Debug.Log("Displaying cooldowns");
+                toDisplay = toDisplay + me.actor.ActorName+ "| " + ((int)((float)me.total / timePassed)).ToString() + "\n";
+                tmpGUI.SetText(toDisplay);
+            }
+        }
+        else{
+            tmpGUI.SetText(toDisplay);
+        }
+    }
+
 }
