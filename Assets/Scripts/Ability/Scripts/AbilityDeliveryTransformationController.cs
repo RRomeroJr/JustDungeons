27fe53056f2b8cc0c35b03c77426df4d35a8fc9b
Pierre Montagne <pierreb.montagne@gmail.com>
2024-03-31 01:18:01 -0400

    Remove redundant null check

--- Assets/Scripts/Ability/Scripts/AbilityDeliveryTransformationController.cs
+++ Assets/Scripts/Ability/Scripts/AbilityDeliveryTransformationController.cs
@@ -36,7 +36,7 @@ public sealed class AbilityDeliveryTransformationController
         // Set the initial spawn of Aoe type to collider center
         if (IsAimingAtCollider && Type is AbilityType.Aoe)
         {
-            if (Target != null && Target.TryGetComponent(out Collider2D targetCollider))
+            if (Target.TryGetComponent(out Collider2D targetCollider))
             {
                 Transform.position = targetCollider.bounds.center;
             }
