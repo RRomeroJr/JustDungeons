f75438e5c9340df56d8b1326ab39153cbfcc0639
RRomeroJr <117.rromero@gmail.com>
2025-01-05 14:48:42 -0500

    Mono to make Npcs cast spells with button press

---
+++ Assets/Scripts/Testing/DevCaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Mirror;
+using UnityEditor;
+using System.IO;
+
+public class DevCaster : MonoBehaviour
+{
+    public Ability_V2 a1;
+    void Update()
+    {
+        if(Input.GetKeyDown(";"))
+        {
+            Debug.Log($"{name} is casting {a1.name}");
+            GetComponent<Actor>().castAbility3(a1, UIManager.playerActor.transform);
+            GetComponent<EnemyController>().autoAttacking = false;
+
+        }
+    }
+}
